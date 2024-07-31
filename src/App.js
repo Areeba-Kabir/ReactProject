81a0a274d772c41f6519398a28bd45899d4ba795
Areeeba <areeebakabir137@gmail.com>
2024-07-31 22:00:28 +0500

    eighth commit (includng react router dom)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-// import About from "./Components/About";
+import About from "./Components/About";
 import Form from "./Components/Form";
 import Navbar from "./Components/Navbar";
 import Alert from "./Components/Alert";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   const [Mode, setMode] = useState("light");
@@ -35,23 +36,34 @@ function App() {
   };
   return (
     <>
-      <Navbar
-        title={"TextAnalyzer"}
-        handleModeClick={handleModeClick}
-        Mode={Mode}
-      />
-      <Alert alert={alert} />
-
-      <div className="container">
-        <Form
-          heading={"Enter the text to Analyze"}
-          toggle={toggle}
-          showAlert={showAlert}
+      <Router>
+        <Navbar
+          title={"TextAnalyzer"}
+          handleModeClick={handleModeClick}
+          Mode={Mode}
         />
-      </div>
-      {/* <div className="container">
-        <About />
-      </div> */}
+        <Alert alert={alert} />
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Form
+                  heading={"Enter the text to Analyze"}
+                  toggle={toggle}
+                  showAlert={showAlert}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/about"
+              element={<About toggle={toggle} showAlert={showAlert} />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/Components/About.js
+++ src/Components/About.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function About() {
+export default function About(props) {
   const [Mode, setMode] = useState("Light Mode");
 
   const [myStyle, setmyStyle] = useState({
@@ -29,31 +29,19 @@ export default function About() {
 
   return (
     <>
-      <div className="form-check form-switch">
-        <input
-          className="form-check-input"
-          type="checkbox"
-          id="flexSwitchCheckChecked"
-          onClick={handleModeClick}
-        />
-        <label className="form-check-label" htmlFor="flexSwitchCheckChecked">
-          {Mode}
-        </label>
-      </div>
-      <h1>About Us</h1>
-      <div className="accordion" id="accordionPanelsStayOpenExample">
-        <div className="accordion-item" style={myStyle}>
+      <h1 className={`text-${props.toggle} text-center my-3`}>About Us</h1>
+      <div className="accordion my-3" id="accordionPanelsStayOpenExample">
+        <div className="accordion-item ">
           <h2 className="accordion-header" id="panelsStayOpen-headingOne">
             <button
-              className="accordion-button"
+              className="accordion-button fw-bolder text-dark"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#panelsStayOpen-collapseOne"
               aria-expanded="true"
               aria-controls="panelsStayOpen-collapseOne"
-              style={myStyle}
             >
-              Accordion Item #1
+              Welcome to TextAnalyzer!
             </button>
           </h2>
           <div
@@ -61,30 +49,32 @@ export default function About() {
             className="accordion-collapse collapse show"
             aria-labelledby="panelsStayOpen-headingOne"
           >
-            <div className="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+            <div className="accordion-body ">
+              TextAnalyzer is your ultimate tool for text manipulation and
+              analysis. Our powerful features allow you to:
+              <ul>
+                <li>Convert text to UPPER CASE or lower case</li>
+                <li>Remove extra spaces for clean and concise text</li>
+                <li>Extract emails effortlessly from your content</li>
+                <li>
+                  Count words and characters to keep track of your writing
+                </li>
+                <li>Preview your text before making any changes</li>
+              </ul>
             </div>
           </div>
         </div>
-        <div className="accordion-item" style={myStyle}>
-          <h2 className="accordion-header" id="panelsStayOpen-headingTwo">
+        <div className="accordion-item">
+          <h2 className="accordion-header " id="panelsStayOpen-headingTwo">
             <button
-              className="accordion-button collapsed"
+              className="accordion-button collapsed fw-bolder text-dark"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#panelsStayOpen-collapseTwo"
               aria-expanded="false"
               aria-controls="panelsStayOpen-collapseTwo"
-              style={myStyle}
             >
-              Accordion Item #2
+              Enhance Your Text Effortlessly
             </button>
           </h2>
           <div
@@ -93,45 +83,11 @@ export default function About() {
             aria-labelledby="panelsStayOpen-headingTwo"
           >
             <div className="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item" style={myStyle}>
-          <h2 className="accordion-header" id="panelsStayOpen-headingThree">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseThree"
-              aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseThree"
-              style={myStyle}
-            >
-              Accordion Item #3
-            </button>
-          </h2>
-          <div
-            id="panelsStayOpen-collapseThree"
-            className="accordion-collapse collapse"
-            aria-labelledby="panelsStayOpen-headingThree"
-          >
-            <div className="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              With TextAnalyzer, you can streamline your text processing needs
+              in a user-friendly interface. Whether you're editing a document,
+              cleaning up data, or simply analyzing text, our tools are designed
+              to make your tasks easier and more efficient. Start using
+              TextAnalyzer today and take control of your text!
             </div>
           </div>
         </div>
